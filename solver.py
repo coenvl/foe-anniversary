86edd945a9bf8c32e5721984e9490255d1237f3d
Coen van Leeuwen <coen.vanleeuwen@tno.nl>
2023-04-04 16:07:12 +0200

    Further optimization on solver

--- solver.py
+++ solver.py
@@ -1,7 +1,7 @@
 """ Solver for the FOE anniversary event """
 from __future__ import annotations
 from math import ceil
-from typing import Callable, Tuple, List, Set
+from typing import Tuple, List, Set, Union
 from enum import Enum
 
 class Part(Enum):
@@ -19,6 +19,16 @@ class Gem():
         self.moves: List[Move] = []
         # Compute binary encoding for hashing and comparing
         self._hash = int((self.level << 3) + (self.locked << 2) + self.part.value)
+
+        self.score = self._compute_score()
+
+    def _compute_score(self) -> float:
+        lock_penalty = self.locked
+        keys = self.part == Part.FULL
+        if self.level == 3:
+            keys *= 3
+            lock_penalty *= 2
+        return keys - .01 * lock_penalty
 
     def can_merge_with(self, other: Gem) -> bool:
         return self.level == other.level and not (self.locked and other.locked)
@@ -47,19 +57,26 @@ class MergedGem(Gem):
                             Part(move[0].part.value | move[1].part.value), False)
         self.moves = move[0].moves + move[1].moves + [(move[0], move[1], self)]
 
-class State():
-    def __init__(self, state: State = None, move: Move = None):
-        if state is None:
-            self.gems: List[Gem] = []
-        else:
-            self.gems = state.gems.copy()
-            if move is not None:
-                self.gems.remove(move[0])
-                self.gems.remove(move[1])
-                self.gems.append(MergedGem(move))
-                self.gems = sorted(self.gems)
+class EmptyState():
+    gems = []
 
-        self._update_score()
+class State():
+    def __init__(self, state: Union[EmptyState, State], move: Move = None):
+        self.gems: List[Gem] = state.gems.copy()
+
+        if move is not None:
+            self.gems.remove(move[0])
+            self.gems.remove(move[1])
+            new_gem = MergedGem(move)
+            self.gems.append(new_gem)
+            self.gems = sorted(self.gems) # Must sort for equality
+
+            # Computing score based on previous. Makes it much faster
+            self.score = state.score - move[0].score - move[1].score + new_gem.score
+            self._tops = state._tops - (move[0].part == Part.TOP) - (move[1].part == Part.TOP) + (new_gem.part == Part.TOP)
+            self._bottoms = state._bottoms - (move[0].part == Part.BOT) - (move[1].part == Part.BOT) + (new_gem.part == Part.BOT)
+            self._free = state._free - (not move[0].locked and not move[1].locked)
+            self.potential_score = ceil(self.score) + 3 * min(self._tops, self._bottoms, self._free)
 
     def _update_score(self):
         keys, tops, bottoms, free, lock_penalty = 0, 0, 0, 0, 0
@@ -73,17 +90,16 @@ class State():
             bottoms += gem.part == Part.BOT
             tops += gem.part == Part.TOP
 
-        self._potential_score = keys + 3 * min(tops, bottoms, free)
-        self._score = keys - .01 * lock_penalty
+        self._tops = tops
+        self._bottoms = bottoms
+        self._free = free
+        self.potential_score = keys + 3 * min(tops, bottoms, free)
+        self.score = keys - .01 * lock_penalty
 
-    def apply(self, move: Tuple[Gem, Gem]):
-        """ Generate a new state by applying the move """
-        return State(self, move)
-
-    def append(self, gem: Gem):
+    def append(self, gem: Gem, count: int = 1):
         """ Appends a gem to the state (for building a new state) """
-        self.gems.append(gem)
-        self._update_score()
+        for _ in range(count):
+            self.gems.append(gem)
 
     def count_keys(self) -> Tuple[int]:
         level3 = sum(gem.level == 2 and gem.part == Part.FULL for gem in self.gems)
@@ -92,22 +108,19 @@ class State():
 
     def show_moves(self):
         def sort_move(move: Move) -> Move:
-            return move if move[1].locked else (move[1], move[0], move[2])
+            return move if move[0] < move[1] else (move[1], move[0], move[2])
 
         for level in range(0,4):
             print(f"==Level {level + 1} Merges==")
+            moves: List[Move] = []
             for gem in self.gems:
                 for move in gem.moves:
                     if move[0].level == level:
                         one, two, result = sort_move(move)
-                        print(f"{str(one):>12} + {str(two):<13} => {result}")
+                        moves.append((one, two, result))
 
-    def score(self) -> float:
-        return self._score
-
-    def potential_score(self) -> int:
-        """ Optimistically estimate a maximum score assuming we can get all parts to level 4 """
-        return self._potential_score
+            for move in sorted(moves):
+                print(f"{str(move[0]):>12} + {str(move[1]):<13} => {move[2]}")
 
     def num_locked(self) -> int:
         """ Number of locked tiles in a state """
@@ -135,20 +148,15 @@ class Solver():
     def __init__(self, locked_bottom, locked_top, free, free_bottom, free_top, free_full):
         self.max_progress = sum(locked_bottom) + sum(locked_top) + locked_bottom[3] + locked_top[3]
 
-        self.start = State()
+        self.start = State(EmptyState())
         for level in range(0,4):
-            for _ in range(locked_bottom[level]):
-                self.start.append(Gem(level, Part.BOT, True))
-            for _ in range(locked_top[level]):
-                self.start.append(Gem(level, Part.TOP, True))
-            for _ in range(free[level]):
-                self.start.append(Gem(level, Part.EMPTY, False))
-            for _ in range(free_bottom[level]):
-                self.start.append(Gem(level, Part.BOT, False))
-            for _ in range(free_top[level]):
-                self.start.append(Gem(level, Part.TOP, False))
-            for _ in range(free_full[level]):
-                self.start.append(Gem(level, Part.FULL, False))
+            self.start.append(Gem(level, Part.BOT, True), locked_bottom[level])
+            self.start.append(Gem(level, Part.TOP, True), locked_top[level])
+            self.start.append(Gem(level, Part.EMPTY, False), free[level])
+            self.start.append(Gem(level, Part.BOT, False), free_bottom[level])
+            self.start.append(Gem(level, Part.TOP, False), free_top[level])
+            self.start.append(Gem(level, Part.FULL, False), free_full[level])
+        self.start._update_score()
 
         self.end_states: Set[State] = set()
         self.best = self.start
@@ -186,7 +194,7 @@ class Solver():
 
         keys = self.best.count_keys()
         res = keys[0] + 3*keys[1]
-        max_potential = ceil(self.start.potential_score())
+        max_potential = ceil(self.start.potential_score)
 
         if res == max_potential:
             print(f"Keys: {res}/{max_potential} (Maximum keys picked up)")
@@ -198,22 +206,21 @@ class Solver():
         remaining_locked = self.best.num_locked()
 
         print(f"Progress: {total_progress}/{self.max_progress} ({remaining_locked} remaining locked gems)")
-        print(f"Score: {self.best.score()}")
         print("Keep in mind these results are only for the selected color")
-        print(len(self.end_states))
+        print(f" -- Score: {self.best.score:0.2f} out of {len(self.end_states)} evaluated games.")
 
     def solve(self) -> State:
         """ Solve the problem """
-        stack = [State(self.start)]
+        stack = [self.start]
         while stack:
             state = stack.pop()
             moves = self._find_possible_moves(state)
             for move in moves:
                 new_state = State(state, move)
 
-                if new_state.potential_score() <= self.best.score() or new_state in self.end_states:
+                if new_state.potential_score <= self.best.score or new_state in self.end_states:
                     continue
-                if new_state.score() > self.best.score():
+                if new_state.score > self.best.score:
                     self.best = new_state
 
                 self.end_states.add(new_state)
@@ -240,9 +247,7 @@ def solve(locked_bottom,locked_top,free,free_bottom,free_top,free_full,silent=Fa
 
     keys = solver.best.count_keys()
 
-
     remaining_progress = solver.best.potential_progress()
     total_progress = solver.start.potential_progress() - remaining_progress
-
 
     return (keys[0] + 3*keys[1], starting, max_keys, total_progress, solver.start.potential_progress(), solver.best.num_locked())
